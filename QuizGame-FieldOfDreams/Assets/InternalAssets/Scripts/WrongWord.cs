9e3175b360f2c093f6c8bd6aa0d2bb060b9d97d5
RimuruDev <Rimuru.Dev@gmail.com>
2022-11-08 14:23:44 +0600

    Create WrongWord.cs

---
+++ QuizGame-FieldOfDreams/Assets/InternalAssets/Scripts/WrongWord.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RimuruDev.SiriusFuture
+{
+    [HelpURL("https://t.me/AbyssMothGames")]
+    public sealed class WrongWord : MonoBehaviour
+    {
+        public Action<Button> OnClickWrongWord;
+
+        [SerializeField, HideInInspector] private GameDataContainer dataContainer;
+        [SerializeField, HideInInspector] private WordHandler wordHandler;
+        [SerializeField, HideInInspector] private CurrentProgress currentProgress;
+        private SaveUserProgress saveUserProgress;
+
+        private void Awake() => CheckRefs();
+
+        public void OnEnable() => OnClickWrongWord += ClickWrongWord;
+        public void OnDisable() => OnClickWrongWord -= ClickWrongWord;
+
+        [System.Diagnostics.Conditional("DEBUG")]
+        private void OnValidate() => CheckRefs();
+
+        private void CheckRefs()
+        {
+            saveUserProgress = new SaveUserProgress();
+
+            if (dataContainer == null)
+                dataContainer = FindObjectOfType<GameDataContainer>();
+
+            if (wordHandler == null)
+                wordHandler = FindObjectOfType<WordHandler>();
+
+            if (currentProgress == null)
+                currentProgress = FindObjectOfType<CurrentProgress>();
+        }
+
+        private void ClickWrongWord(Button button)
+        {
+            dataContainer.GetHeaderValue.NumberOfAttempts -= 1;
+
+            button.interactable = false;
+            button.gameObject.GetComponent<Image>().color = new Color(74, 70, 69, 0);
+            button.transform.GetChild(0).gameObject.SetActive(false);
+
+            if (dataContainer.GetHeaderValue.NumberOfAttempts == 0)
+            {
+                saveUserProgress.OnSaveScore(0);
+                UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+}
